d71543b9db42d8a2ce0c5a80a3be1094d555efee
Vladimir Evgeniev <papagal93@gmail.com>
2018-02-28 01:31:57 +0200

    function practice

--- functions-ejs.js
+++ functions-ejs.js
@@ -1,1 +1,37 @@
+function min(a, b) {
+	if (a > b){
+		return b
+	} else {
+		return a
+	}
+}
 
+console.log(min(3, 10));
+
+
+function isEven(n){
+ if (n % 2 == 0) {
+ 	return true;
+ } else if (n % 2 == 1) {
+ 	return false;
+} else {
+	return "Your number is negative.";
+}}
+
+console.log(isEven(-5));
+
+/*function countBs(x){
+
+	x = " ";
+	
+	for(var count = 0; count < dude.length; count++){
+	
+	}
+	
+
+}
+console.log(countBs(x));
+
+function countChar(){
+
+}
